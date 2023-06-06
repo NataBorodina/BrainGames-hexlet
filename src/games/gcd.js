b55a005d4c8bd9c9e6a74e9931c4f1c622d72d94
NataliaBorodina <borodinn@list.ru>
2023-06-06 15:33:33 +0600

    edit gcd  game

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,19 +1,18 @@
-import readlineSync from 'readline-sync';
 import { getRandomNum, gameLogic } from './index.js';
 
 const description = 'Find the greatest common divisor of given numbers.';
 
 const getGcd = (a, b) => ((a % b) ? getGcd(b, a % b) : Math.abs(b));
 
-const game = () => {
+const gameRound = () => {
   const number1 = getRandomNum(100);
   const number2 = getRandomNum(100);
-  console.log(`Question: ${number1} ${number2}`);
-  const userAnswer = readlineSync.question('Your answer: ');
+  const question = `${number1} ${number2}`;
   const correctAnswer = String(getGcd(number1, number2));
-  return { userAnswer, correctAnswer };
+
+  return [question, correctAnswer];
 };
 
-export const takeCorrectAnswer = ({ correctAnswer }) => correctAnswer;
-
-export const playGcd = () => { gameLogic(description, game, takeCorrectAnswer); };
+export default () => {
+  gameLogic(description, gameRound);
+};

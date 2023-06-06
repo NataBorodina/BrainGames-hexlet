41f751df2376e5ad4fa49fd8e7994cc1368d589d
NataliaBorodina <borodinn@list.ru>
2023-06-06 15:55:20 +0600

    edit progression game

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,13 +1,11 @@
-import readlineSync from 'readline-sync';
 import { getRandomNum, gameLogic } from './index.js';
 
 const description = 'What number is missing in the progression?';
 
-const takeArray = (start, step) => {
-  const arr = [start];
-  for (let i = 1; arr.length <= 9; i += 1) {
-    const item = arr[i - 1] + step;
-    arr.push(item);
+const takeArray = (start, step, length) => {
+  const arr = [];
+  for (let i = 0; i < length; i += 1) {
+    arr.push(start + step * i);
   }
   return arr;
 };
@@ -18,18 +16,18 @@ const takeGameArray = (arr, spaseIndex) => {
   return gameArray.join(' ');
 };
 
-const game = () => {
+const gameRound = () => {
   const start = getRandomNum(100);
   const step = getRandomNum(100);
-  const array = takeArray(start, step);
+  const length = 10;
+  const array = takeArray(start, step, length);
   const spaseIndex = Math.floor(Math.random() * 9) + 1;
   const correctAnswer = String(array[spaseIndex]);
   const gameArray = takeGameArray(array, spaseIndex);
-  console.log(`Question: ${gameArray}`);
-  const userAnswer = readlineSync.question('Your answer: ');
-  return { userAnswer, correctAnswer };
+  const question = `Question: ${gameArray}`;
+  return [question, correctAnswer];
 };
 
-export const takeCorrectAnswer = ({ correctAnswer }) => correctAnswer;
-
-export const playProgression = () => { gameLogic(description, game, takeCorrectAnswer); };
+export default () => {
+  gameLogic(description, gameRound);
+};
